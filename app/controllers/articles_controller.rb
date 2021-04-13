21328cf80474c301525e4de3debacd1b0b5a9b1e
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-04-13 21:00:32 +0200

    added Comment model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,7 +14,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      redirect_to @bookmark
+      redirect_to @article
     else
       render :new
     end
@@ -32,6 +32,13 @@ class ArticlesController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
   end
 
   private
